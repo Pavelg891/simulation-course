82f27e55a8f83a5c0f9336bb1c8d4e1e0bee63fa
Pavelg891 <golobokov.pa@gmail.com>
2026-04-03 07:05:09 +0700

    lab5

---
+++ lab05/lab5/Form1.Designer.cs
@@ -0,0 +1,167 @@
+﻿namespace lab5
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.textBoxYesNo = new System.Windows.Forms.TextBox();
+            this.buttonAnswer = new System.Windows.Forms.Button();
+            this.labelYesNo = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.textBoxMagic8Ball = new System.Windows.Forms.TextBox();
+            this.buttonMagic8Ball = new System.Windows.Forms.Button();
+            this.labelMagic8Ball = new System.Windows.Forms.Label();
+            this.buttonStats = new System.Windows.Forms.Button();
+            this.labelStats = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // textBoxYesNo
+            // 
+            this.textBoxYesNo.Location = new System.Drawing.Point(63, 48);
+            this.textBoxYesNo.Name = "textBoxYesNo";
+            this.textBoxYesNo.Size = new System.Drawing.Size(165, 20);
+            this.textBoxYesNo.TabIndex = 0;
+            this.textBoxYesNo.Text = "Пойти сегодня в университет?";
+            // 
+            // buttonAnswer
+            // 
+            this.buttonAnswer.Location = new System.Drawing.Point(63, 95);
+            this.buttonAnswer.Name = "buttonAnswer";
+            this.buttonAnswer.Size = new System.Drawing.Size(75, 23);
+            this.buttonAnswer.TabIndex = 1;
+            this.buttonAnswer.Text = "ответ";
+            this.buttonAnswer.UseVisualStyleBackColor = true;
+            this.buttonAnswer.Click += new System.EventHandler(this.buttonAnswer_Click);
+            // 
+            // labelYesNo
+            // 
+            this.labelYesNo.AutoSize = true;
+            this.labelYesNo.Location = new System.Drawing.Point(60, 143);
+            this.labelYesNo.Name = "labelYesNo";
+            this.labelYesNo.Size = new System.Drawing.Size(0, 13);
+            this.labelYesNo.TabIndex = 2;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(60, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(116, 13);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Скажи \"да\" или \"нет\"";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(376, 9);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(103, 13);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "Шар предсказаний";
+            // 
+            // textBoxMagic8Ball
+            // 
+            this.textBoxMagic8Ball.Location = new System.Drawing.Point(379, 48);
+            this.textBoxMagic8Ball.Name = "textBoxMagic8Ball";
+            this.textBoxMagic8Ball.Size = new System.Drawing.Size(165, 20);
+            this.textBoxMagic8Ball.TabIndex = 5;
+            this.textBoxMagic8Ball.Text = "Пойти сегодня в университет?";
+            // 
+            // buttonMagic8Ball
+            // 
+            this.buttonMagic8Ball.Location = new System.Drawing.Point(379, 95);
+            this.buttonMagic8Ball.Name = "buttonMagic8Ball";
+            this.buttonMagic8Ball.Size = new System.Drawing.Size(75, 23);
+            this.buttonMagic8Ball.TabIndex = 6;
+            this.buttonMagic8Ball.Text = "ответ";
+            this.buttonMagic8Ball.UseVisualStyleBackColor = true;
+            this.buttonMagic8Ball.Click += new System.EventHandler(this.buttonMagic8Ball_Click);
+            // 
+            // labelMagic8Ball
+            // 
+            this.labelMagic8Ball.AutoSize = true;
+            this.labelMagic8Ball.Location = new System.Drawing.Point(376, 143);
+            this.labelMagic8Ball.Name = "labelMagic8Ball";
+            this.labelMagic8Ball.Size = new System.Drawing.Size(0, 13);
+            this.labelMagic8Ball.TabIndex = 7;
+            // 
+            // buttonStats
+            // 
+            this.buttonStats.Location = new System.Drawing.Point(583, 95);
+            this.buttonStats.Name = "buttonStats";
+            this.buttonStats.Size = new System.Drawing.Size(75, 23);
+            this.buttonStats.TabIndex = 8;
+            this.buttonStats.Text = "статистика";
+            this.buttonStats.UseVisualStyleBackColor = true;
+            this.buttonStats.Click += new System.EventHandler(this.buttonStats_Click);
+            // 
+            // labelStats
+            // 
+            this.labelStats.AutoSize = true;
+            this.labelStats.Location = new System.Drawing.Point(580, 143);
+            this.labelStats.Name = "labelStats";
+            this.labelStats.Size = new System.Drawing.Size(0, 13);
+            this.labelStats.TabIndex = 9;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.labelStats);
+            this.Controls.Add(this.buttonStats);
+            this.Controls.Add(this.labelMagic8Ball);
+            this.Controls.Add(this.buttonMagic8Ball);
+            this.Controls.Add(this.textBoxMagic8Ball);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.labelYesNo);
+            this.Controls.Add(this.buttonAnswer);
+            this.Controls.Add(this.textBoxYesNo);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBoxYesNo;
+        private System.Windows.Forms.Button buttonAnswer;
+        private System.Windows.Forms.Label labelYesNo;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox textBoxMagic8Ball;
+        private System.Windows.Forms.Button buttonMagic8Ball;
+        private System.Windows.Forms.Label labelMagic8Ball;
+        private System.Windows.Forms.Button buttonStats;
+        private System.Windows.Forms.Label labelStats;
+    }
+}
+
---
+++ lab05/lab5/Form1.cs
@@ -0,0 +1,100 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace lab5
+{
+    public partial class Form1 : Form
+    {
+        Random rnd = new Random();
+        private string[] Answers =
+        {
+            "Да",
+            "Определённо да",
+            "Без сомнений",
+            "Можешь быть уверен",
+            "Скорее всего",
+            "Нет",
+            "Маловероятно",
+            "Не думаю",
+            "Определённо нет",
+            "Очень сомнительно",
+            "Спроси позже",
+            "Сейчас нельзя сказать",
+            "Ответ неясен",
+            "Сконцентрируйся и спроси снова"
+        };
+        double[] probabilities;
+        bool YesNo(double p)
+        {
+            double value = rnd.NextDouble();
+            if (value < p)
+                return true;
+            return false;
+        }
+        public Form1()
+        {
+            InitializeComponent();
+            probabilities = new double[Answers.Length];
+            for (int i = 0; i < Answers.Length; i++) probabilities[i] = 1.0 / Answers.Length;
+        }
+
+        int Magic8Ball()
+        {
+            double A = rnd.NextDouble();
+            for (int k = 0; k < Answers.Length; k++)
+            {
+                A = A - probabilities[k];
+                if (A <= 0) return k;
+            }
+            return -1;
+        }
+        private void buttonAnswer_Click(object sender, EventArgs e)
+        {
+            double p = 0.7;
+            if (YesNo(p)) labelYesNo.Text = "Да";
+            else labelYesNo.Text = "Нет";
+        }
+
+        private void buttonMagic8Ball_Click(object sender, EventArgs e)
+        {
+            int k = Magic8Ball();
+            if (k != -1) labelMagic8Ball.Text = Answers[k];
+        }
+
+        private void buttonStats_Click(object sender, EventArgs e)
+        {
+            int N = 10000;
+
+            int[] statistics = new int[Answers.Length];
+
+            for (int i = 0; i < statistics.Length; i++)
+                statistics[i] = 0;
+
+            for (int n = 0; n < N; n++)
+            {
+                int k = Magic8Ball();
+                if (k != -1)
+                    statistics[k]++;
+            }
+
+            StringBuilder sb = new StringBuilder();
+            sb.AppendLine($"N = {N}");
+            sb.AppendLine();
+            for (int i = 0; i < Answers.Length; i++)
+            {
+                double freq = (double)statistics[i] / N;
+                sb.AppendLine($"{Answers[i]}: {freq:F4}");
+            }
+            labelStats.Text = sb.ToString();
+        }
+    }
+
+}
